12bc2806e1890a941dd103356128ef86ae3d8db9
tchauke023 <tchauke023@student.wethinkcode.co.za>
2024-07-24 11:01:27 +0200

    placed everything into separate methods

--- src/main/java/com/chauke/Calculator.java
+++ src/main/java/com/chauke/Calculator.java
@@ -9,96 +9,30 @@ public class Calculator implements ActionListener {
 
     private JFrame frame;
     private JTextField textField;
-    private JButton[] numberButtons = new JButton[10];
-    private JButton[] functionButtons = new JButton[9];
+    private final JButton[] numberButtons = new JButton[10];
+    private final JButton[] functionButtons = new JButton[9];
     private JButton addButton, subButton, multiplyButton, divideButton, negPosButton;
     private JButton decimalButton, equalButton, deleteButton, clearButton;
     private JPanel panel;
 
     private final Font font = new Font("Ink Free", Font.BOLD,30);
 
-    private double num1, num2, result = 0;
+    private double num1;
+    private double result = 0;
     private char operator;
 
     public Calculator(){
-
-//        frame = new JFrame("Calculator");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setSize(420,550);
-//        frame.setLayout(null);
-//        frame = configureFrame();
         configureFrame();
+        textField = configureTextField();
 
-//        textField = new JTextField();
-//        textField.setBounds(50,25,300,50);
-//        textField.setFont(font);
-//        textField.setEditable(false);
+        setOperatorButtons();
 
-        addButton = new JButton("+");
-        subButton = new JButton("-");
-        multiplyButton = new JButton("*");
-        divideButton = new JButton("/");
-        clearButton = new JButton("Clear");
-        deleteButton = new JButton("<-");
-        equalButton = new JButton("=");
-        decimalButton = new JButton(".");
-        negPosButton = new JButton("-/+");
+        configureNumberButtons();
 
-        functionButtons[0] = addButton;
-        functionButtons[1] = subButton;
-        functionButtons[2] = multiplyButton;
-        functionButtons[3] = divideButton;
-        functionButtons[4] = decimalButton;
-        functionButtons[5] = equalButton;
-        functionButtons[6] = deleteButton;
-        functionButtons[7] = clearButton;
-        functionButtons[8] = negPosButton;
+        panel = configurePanel();
+        addToPanel();
 
-        for (int i = 0; i < 9; i++) {
-            functionButtons[i].addActionListener(this);
-            functionButtons[i].setFont(font);
-            functionButtons[i].setFocusable(false);
-        }
-
-        for (int i = 0; i < 10; i++) {
-            numberButtons[i] = new JButton(String.valueOf(i));
-            numberButtons[i].addActionListener(this);
-            numberButtons[i].setFont(font);
-            numberButtons[i].setFocusable(false);
-        }
-
-        negPosButton.setBounds(50,430,100,50);
-        deleteButton.setBounds(150,430,100,50);
-        clearButton.setBounds(250,430,100,50);
-
-        panel = new JPanel();
-        panel.setBounds(50,100,300,300);
-        panel.setLayout(new GridLayout(4,4,10,10));
-//        panel.setBackground(Color.GRAY);
-
-        panel.add(numberButtons[1]);
-        panel.add(numberButtons[2]);
-        panel.add(numberButtons[3]);
-        panel.add(addButton);
-        panel.add(numberButtons[4]);
-        panel.add(numberButtons[5]);
-        panel.add(numberButtons[6]);
-        panel.add(subButton);
-        panel.add(numberButtons[7]);
-        panel.add(numberButtons[8]);
-        panel.add(numberButtons[9]);
-        panel.add(multiplyButton);
-        panel.add(decimalButton);
-        panel.add(numberButtons[0]);
-        panel.add(equalButton);
-        panel.add(divideButton);
-
-        frame.add(panel);
-        frame.add(negPosButton);
-        frame.add(deleteButton);
-        frame.add(clearButton);
-        frame.add(textField);
-        frame.setVisible(true);
+        addToFrame();
     }
 
     @Override
@@ -132,7 +66,7 @@ public class Calculator implements ActionListener {
             textField.setText("");
         }
         if (e.getSource() == equalButton){
-            num2 = Double.parseDouble(textField.getText());
+            double num2 = Double.parseDouble(textField.getText());
 
             switch (operator) {
                 case '+':
@@ -184,5 +118,83 @@ public class Calculator implements ActionListener {
         textField.setEditable(false);
 
         return textField;
+    }
+
+    private void setOperatorButtons(){
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        multiplyButton = new JButton("*");
+        divideButton = new JButton("/");
+        clearButton = new JButton("Clear");
+        deleteButton = new JButton("<-");
+        equalButton = new JButton("=");
+        decimalButton = new JButton(".");
+        negPosButton = new JButton("-/+");
+
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = multiplyButton;
+        functionButtons[3] = divideButton;
+        functionButtons[4] = decimalButton;
+        functionButtons[5] = equalButton;
+        functionButtons[6] = deleteButton;
+        functionButtons[7] = clearButton;
+        functionButtons[8] = negPosButton;
+
+        for (int i = 0; i < 9; i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(font);
+            functionButtons[i].setFocusable(false);
+        }
+
+        negPosButton.setBounds(50,430,100,50);
+        deleteButton.setBounds(150,430,100,50);
+        clearButton.setBounds(250,430,100,50);
+    }
+
+    private void configureNumberButtons(){
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(font);
+            numberButtons[i].setFocusable(false);
+        }
+    }
+
+    private JPanel configurePanel(){
+        panel = new JPanel();
+        panel.setBounds(50,100,300,300);
+        panel.setLayout(new GridLayout(4,4,10,10));
+
+        return panel;
+    }
+
+    private void addToPanel(){
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(multiplyButton);
+        panel.add(decimalButton);
+        panel.add(numberButtons[0]);
+        panel.add(equalButton);
+        panel.add(divideButton);
+
+    }
+
+    private void addToFrame(){
+        frame.add(panel);
+        frame.add(negPosButton);
+        frame.add(deleteButton);
+        frame.add(clearButton);
+        frame.add(textField);
+        frame.setVisible(true);
     }
 }
