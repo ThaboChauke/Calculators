343362ece7fd5a85bee10743f7e5eee575ee6e68
ThaboChauke <chaukethabo16@gmail.com>
2024-09-15 14:52:08 +0200

    basic calculator functionality for 2 numbers

--- javascript-calculator/scripts/script.js
+++ javascript-calculator/scripts/script.js
@@ -1,5 +1,31 @@
 let num1 = 8
 let num2 = 2
+let sum = document.getElementById("sum-el")
+
 
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+
+function add() {
+    sum.textContent = "Sum: "
+    result = num1 + num2
+    sum.textContent += result
+}
+
+function subtract() {
+    sum.textContent = "Sum: "
+    result = num1 - num2
+    sum.textContent += result
+}
+
+function multiply() {
+    sum.textContent = "Sum: "
+    result = num1 * num2
+    sum.textContent += result
+}
+
+function divide() {
+    sum.textContent = "Sum: "
+    result = num1 / num2
+    sum.textContent += result
+}
